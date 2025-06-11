7bba8127dcbd36ef135bd20cd5f109b10e69ad9e
Aishsara23 <a.sankar.narayanan@accenture.com>
2025-06-11 15:16:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def greet(name):
-    if name:
-        print(f"Hello, {name}!")
-    else:
-        print("Hello, Guest!")
+class Calculator:
+    def add(self, a, b):
+        return a + b
 
-greet("Aishwarya")
+    def divide(self, a, b):
+        if b == 0:
+            return "Cannot divide by zero"
+        return a / b
+
+calc = Calculator()
+print(calc.add(10, 20))
+print(calc.divide(10, 0))
